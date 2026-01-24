460b01a93aff9b5dcd5ee12912498832978cbb51
samsam-git <samswnjhay169@gmail.comsource>
2026-01-24 19:45:55 +0300

    vendor registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
+
+from vendor.forms import VendorForm
+from vendor.models import Vendor
 
 from django.contrib import messages
 
@@ -27,9 +30,10 @@ def registerUser(request):
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
 
-            User.objects.create_user(first_name=first_name, last_name=last_name,
-                                     username=username, email=email, password=password)
-
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,
+                                            username=username, email=email, password=password)
+            user.role = User.CUSTOMER
+            user.save()
             messages.success(
                 request, "Your account has been registered successfully.")
 
@@ -43,3 +47,43 @@ def registerUser(request):
     }
 
     return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            user = User.objects.create_user(first_name=first_name, last_name=last_name,
+                                            username=username, email=email, password=password)
+            user.role = User.VENDOR
+            user.save()
+            # Vendor creation
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+
+            messages.success(
+                request, "Your account has been registered successfully. please wait for the approvale.")
+            return redirect('registerVendor')
+        else:
+            print('Invalid Form')
+            print(form.errors)
+
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
